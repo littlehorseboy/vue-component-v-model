af4203546fefa37778ffaeaaea2322c48df341e6
littlehorseboy <littlehorseboy@gmail.com>
2020-02-01 01:19:02 +0800

    init

---
+++ src/components/Input.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input :value="inputValue" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    inputValue: {
+      type: String,
+    },
+  },
+};
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Input inputValue="inputValue" />
+  </div>
+</template>
+
+<script>
+import Input from '@/components/Input.vue'
+
+export default {
+  components: {
+    Input,
+  },
+  data() {
+    return {
+      inputValue: '隨便給個值',
+    };
+  },
+};
+</script>
